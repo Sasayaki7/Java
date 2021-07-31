0ca8beb99aca0e41244a7af92a67ec8ba9ab2572
Sasayaki7 <50351133+Sasayaki7@users.noreply.github.com>
2021-07-31 17:15:18 -0400

    initial java

---
+++ javaFun/IntroProgram.java
@@ -0,0 +1,7 @@
+public class IntroProgram {
+    public static void main(String[] args){
+        System.out.println("My name is Sasa");
+        System.out.println("I am 15 years old");
+        System.out.println("My hometown in Livonia, Michigan");
+    }
+}
